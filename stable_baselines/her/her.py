8023bbce9bc6bbc194130cb5b596d014e3e69dd7
Antonin RAFFIN <antonin.raffin@ensta.org>
2019-04-23 20:48:55 +0200

    Fix HER learning method

--- stable_baselines/her/her.py
+++ stable_baselines/her/her.py
@@ -102,8 +102,9 @@ class HER(BaseRLModel):
 
     def learn(self, total_timesteps, callback=None, seed=None, log_interval=100, tb_log_name="HER",
               reset_num_timesteps=True):
-        return self.model.learn(total_timesteps, callback=None, seed=None, log_interval=100, tb_log_name="HER",
-                                reset_num_timesteps=True, replay_wrapper=self.replay_wrapper)
+        return self.model.learn(total_timesteps, callback=callback, seed=seed, log_interval=log_interval,
+                                tb_log_name=tb_log_name, reset_num_timesteps=reset_num_timesteps,
+                                replay_wrapper=self.replay_wrapper)
 
     def _check_obs(self, observation):
         if isinstance(observation, dict):
